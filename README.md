618fe5c77333a71df288caea75849ff75956e303
Nathan Reis <53984490+NathanReis@users.noreply.github.com>
2021-04-08 14:04:17 -0300

    Arquivos

---
+++ README.md
@@ -0,0 +1,5 @@
+# Cara ou coroa
+
+Resolva conflitos entre duas partes com apenas um click.
+
+**Obs.**: É possível obter um resultado viciado.
---
+++ js/index.js
@@ -0,0 +1,79 @@
+function calculatePercentClick(dimensionButton, coordinatesClick) {
+    /**
+     * |----------|----x     |
+     * ^          ^          ^
+     * |          |          |
+     * |          |          -> Fim do botão
+     * |          |
+     * |          -> Início do botão
+     * |
+     * -> Início da tela
+     */
+
+    // Remove distância do início da tela até início do botão
+    let lengthClick = coordinatesClick.x - dimensionButton.coordinateXOnScreen;
+
+    return (lengthClick / dimensionButton.width) * 100;
+}
+
+function handleThrowCoin(event) {
+    document.querySelector("#throw-coin").removeEventListener("click", handleThrowCoin);
+
+    document.querySelector("#icon-crown").style.display = "none";
+    document.querySelector("#icon-face").style.display = "none";
+
+    let iconQuestion = document.querySelector("#icon-question");
+
+    iconQuestion.style.display = "inline-block";
+    iconQuestion.classList.add("throwCoin");
+
+    setTimeout(() => {
+        let coordinatesClick = getCoordinatesClick(event);
+        let dimensionButton = getDimensionButton(event.target);
+
+        showResult(dimensionButton, coordinatesClick);
+    }, 2000);
+}
+
+function getCoordinatesClick(event) {
+    let { x, y } = event;
+
+    return { x, y };
+}
+
+function getDimensionButton(button) {
+    return {
+        height: button.offsetHeight,
+        width: button.offsetWidth,
+        coordinateXOnScreen: button.offsetLeft,
+        coordinateYOnScreen: button.offsetTop
+    };
+}
+
+function showResult(dimensionButton, coordinatesClick) {
+    let iconQuestion = document.querySelector("#icon-question");
+
+    iconQuestion.style.display = "none";
+    iconQuestion.classList.remove("throwCoin");
+
+    let percentClick = calculatePercentClick(dimensionButton, coordinatesClick);
+    let limitPercent = 4; // Valor de 50% para cara e coroa
+
+    if (percentClick >= 25 && percentClick <= 44) {
+        limitPercent = 2; // Valor de 80% para cara
+    } else if (percentClick >= 56 && percentClick <= 75) {
+        limitPercent = 8; // Valor de 80% para coroa
+    }
+
+    let drawNumber = Math.floor(Math.random() * 10);
+
+    if (drawNumber > limitPercent) {
+        document.querySelector("#icon-face").style.display = "inline-block";
+    } else {
+        document.querySelector("#icon-crown").style.display = "inline-block";
+    }
+
+    document.querySelector("#throw-coin").addEventListener("click", handleThrowCoin);
+}
+
+document.querySelector("#throw-coin").addEventListener("click", handleThrowCoin);
